c288faca347ce43bd1968b312c8f1c969e98e26a
MillHidden <jeremy.hindenoch@gmail.com>
2016-06-16 15:03:05 +0200

    Add files via upload

--- src/OC/PlatformBundle/ParamConverter/JsonParamConverter.php
+++ src/OC/PlatformBundle/ParamConverter/JsonParamConverter.php
@@ -11,23 +11,19 @@ class JsonParamConverter implements ParamConverterInterface
 {
   function supports(ParamConverter $configuration)
   {
-    // Si le nom de l'argument du contrôleur n'est pas "json", on n'applique pas le convertisseur
-    if ('json' !== $configuration->getName()) {
-      return false;
-    }
+  	if ('json' !== $configuration->getName()) {
+  		return false;
+  	}
 
-    return true;
+  	return true;
   }
 
   function apply(Request $request, ParamConverter $configuration)
   {
-    // On récupère la valeur actuelle de l'attribut
-    $json = $request->attributes->get('json');
+  	$json = $request->attributes->get('json');
 
-    // On effectue notre action : le décoder
-    $json = json_decode($json, true);
+  	$json = json_decode($json, true);
 
-    // On met à jour la nouvelle valeur de l'attribut
-    $request->attributes->set('json', $json);
+  	$request->attributes->set('json', $json);
   }
 }
